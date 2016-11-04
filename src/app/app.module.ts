a15c5ff585d60f81cb68eb5dcd94df68dc9db012
Adam Martin <adam.martin@1insurer.com>
2016-11-04 17:00:21 +0000

    Added navigation

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -5,23 +5,27 @@ import {HttpModule} from '@angular/http';
 
 import {AppComponent} from './app.component';
 import {BallComponent} from './ball/ball.component';
-import {ButtonsModule} from 'ng2-bootstrap/ng2-bootstrap';
+import {ButtonsModule, DropdownModule, CollapseModule} from 'ng2-bootstrap/ng2-bootstrap';
 import {HomeComponent} from './pages/home/home.component';
 import {AnimationComponent} from './pages/animation/animation.component';
 import routes from './app.routes';
+import { NavComponent } from './sections/nav/nav.component';
 
 @NgModule({
     declarations: [
         AppComponent,
         BallComponent,
         HomeComponent,
-        AnimationComponent
+        AnimationComponent,
+        NavComponent
     ],
     imports: [
         BrowserModule,
         FormsModule,
         HttpModule,
         ButtonsModule,
+        CollapseModule,
+        DropdownModule,
         routes
     ],
     providers: [],
