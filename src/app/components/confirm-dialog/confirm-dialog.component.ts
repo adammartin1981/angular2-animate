4955b0ce14edfc9172216d9e9dc2dc6356922152
Adam Martin <adam.martin@ctt.co.uk>
2017-03-02 15:38:10 +0000

    All demo confirm dialog

---
+++ src/app/components/confirm-dialog/confirm-dialog.component.ts
@@ -0,0 +1,18 @@
+import { Component } from '@angular/core';
+import { MdDialogRef } from '@angular/material';
+
+@Component({
+    selector: 'app-confirm-dialog',
+    templateUrl: 'confirm-dialog.component.html',
+    styleUrls: ['confirm-dialog.component.css']
+})
+export class ConfirmDialogComponent {
+
+    public title: string;
+    public message: string;
+
+    constructor(
+        public dialogRef: MdDialogRef<ConfirmDialogComponent>) {
+    }
+
+}
